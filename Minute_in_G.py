b28df1d380a301266d0e38d8bf16c556cac4d888
A01703363 <A01703363@itesm.mx>
2021-03-18 22:35:13 -0600

    Agregamos mas notas de las partitura, incluyendo las que van en cuarto

--- Minute_in_G.py
+++ Minute_in_G.py
@@ -2,10 +2,27 @@
 from music21 import note, stream, meter
 
 
-notes  = ['C5', 'F', 'G', 'A', 'Bb', 'C5', 'F', 'F', 'D5', 'Bb', 
-         'C5', 'D5', 'E5', 'F5', 'F', 'F']
+notes  = ['C5', 'F', 'G', 'A', 'Bb', 
+        'C5', 'F', 'F', 
+        'D5', 'Bb', 'C5', 'D5', 'E5', 
+        'F5', 'F', 'F',
+        'Bb', 'C5', 'Bb', 'A', 'G',
+        'A', 'Bb', 'A', 'G', 'F',
+        'E', 'F', 'G', 'A', 'F',
+        'G',
+        'C5', 'F', 'G', 'A', 'Bb', 
+        'C5', 'F', 'F', 
+        'D5', 'Bb', 'C5', 'D5', 'E5', 
+        'F5', 'F', 'F',
+        'Bb', 'C5', 'Bb', 'A', 'G',
+        'A', 'Bb', 'A', 'G', 'F',
+        'E', 'F', 'G', 'A', 'F',
+        'F']
+
 # Posiciones de las notas que duran 1/4
-cuartos = [0, 5, 6, 7, 8, 13, 14, 15] 
+cuartos = [0, 5, 6, 7, 8, 13, 14, 15, 16, 21, 
+          26, 31, 32, 37, 38, 39, 40, 45, 46, 
+          47, 48, 53, 58, 63] 
 st = stream.Stream()
 for i in range(len(notes)):
   new_note = note.Note(notes[i])
@@ -13,4 +30,4 @@ for i in range(len(notes)):
     new_note.duration.quarterLength = 0.5  
   st.append(new_note)
   st.insert(0, meter.TimeSignature('3/4'))
-st.write('midi', fp="cancion14.mid")
+st.write('midi', fp="cancion15.mid")
